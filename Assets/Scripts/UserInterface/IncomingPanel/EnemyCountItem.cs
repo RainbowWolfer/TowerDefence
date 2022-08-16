ed4d978f32aaeb98d55b29c0795d625514c424e8
RainbowWolfer <RainbowWolfer@outlook.com>
2022-08-16 23:15:20 +0800

    Update

---
+++ Assets/Scripts/UserInterface/IncomingPanel/EnemyCountItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TowerDefence.UserInterface.IncomingPanel {
+	public class EnemyCountItem: MonoBehaviour {
+		[SerializeField]
+		private Image border;
+		[SerializeField]
+		private Image content;
+		[SerializeField]
+		private TextMeshProUGUI countText;
+
+
+	}
+}
---
+++ Assets/Scripts/UserInterface/IncomingPanel/EnemyCountsManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDefence.UserInterface.IncomingPanel {
+	public class EnemyCountsManager: MonoBehaviour {
+		[SerializeField]
+		private GameObject itemPrefab;
+		[SerializeField]
+		private List<EnemyCountItem> items = new List<EnemyCountItem>();
+
+		private void Start() {
+			Clear();
+		}
+
+
+		public void Clear() {
+			items.Clear();
+			for(int i = 0; i < transform.childCount; i++) {
+				Destroy(transform.GetChild(i).gameObject);
+			}
+		}
+	}
+}
